fb5df15c261ae1801a966ea01e96f97237c5834c
Danial Bleile <danbleile@gmail.com>
2021-08-25 05:42:21 -0700

    Bug Fix | Fix social share buttons

--- blocks/wsuwp-post-social/templates/default.php
+++ blocks/wsuwp-post-social/templates/default.php
@@ -1,27 +1,22 @@
 <ul class="<?php echo esc_attr( $wrapper_classes ); ?>">
 	<?php if ( empty( $attrs['hideTwitter]'] ) ) : ?>
 	<li class="wsu-social-icons__twitter">
-		<a href="https://twitter.com/icons?text=&url=<?php echo esc_url( $attrs['link'] ); ?>" title="icons on Twitter"></a>
+		<a href="https://twitter.com/intent/tweet?text=<?php echo esc_url( $attrs['title'] ); ?>&url=<?php echo esc_url( $attrs['link'] ); ?>" title="Share on Twitter"></a>
 	</li>
 	<?php endif; ?>
 	<?php if ( empty( $attrs['hideFacebook]'] ) ) : ?>
 	<li class="wsu-social-icons__faceblook">
-		<a href="https://www.facebook.com/iconsr/iconsr.php?u=<?php echo esc_url( $attrs['link'] ); ?>" title="icons on FaceBook"></a>
+		<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo esc_url( $attrs['link'] ); ?>" title="Share on FaceBook"></a>
 	</li>
 	<?php endif; ?>
 	<?php if ( empty( $attrs['hideLinkedin]'] ) ) : ?>
 	<li class="wsu-social-icons__linkedin">
-		<a href="https://www.linkedin.com/iconsArticle?mini=true&url=<?php echo esc_url( $attrs['link'] ); ?>" title="icons on Linkedin"></a>
-	</li>
-	<?php endif; ?>
-	<?php if ( empty( $attrs['hidePintrest]'] ) ) : ?>
-	<li class="wsu-social-icons__pintrest">
-		<a href="https://pinterest.com/pin/create/button/?description=&media=&url=<?php echo esc_url( $attrs['link'] ); ?>" title="icons on Linkedin"></a>
+		<a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo esc_url( $attrs['link'] ); ?>" title="Share on Linkedin"></a>
 	</li>
 	<?php endif; ?>
 	<?php if ( empty( $attrs['hideEmail]'] ) ) : ?>
 	<li class="wsu-social-icons__email">
-		<a href="mailto:?subject=[title]&body=Check out this: <?php echo esc_url( $attrs['link'] ); ?>" title="icons with email"></a>
+		<a href="mailto:?subject=Shared%20With%20You:%20<?php echo urldecode( $attrs['title'] ); ?>&body=<?php echo esc_url( $attrs['bodyText'] ); ?> <?php echo esc_url( $attrs['link'] ); ?>" title="share with email"></a>
 	</li>
 	<?php endif; ?>
 </ul>
