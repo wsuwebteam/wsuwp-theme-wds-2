709be177e253f00982cc6c25bc4119b99e582e65
Danial Bleile <danbleile@gmail.com>
2021-10-14 16:39:10 -0700

    Bug Fix | Archive template 1.0.5

--- archive.php
+++ archive.php
@@ -1,21 +1,26 @@
 <?php namespace WSUWP\Theme\WDS; ?>
+<?php
+
+$post_type = ( is_post_type_archive() ) ? get_post_type() : '';
+
+;?>
 
 <?php get_header(); ?>
 <!-- GLOBAL CONTAINER:START -->
 <div class="wsu-wrapper-global">
-    <?php Template::render( 'block-templates/header-global', get_post_type() ); ?>
-    <?php Template::render( 'block-templates/navigation-site-vertical', get_post_type() ); ?>
+    <?php Template::render( 'block-templates/header-global', $post_type ); ?>
+    <?php Template::render( 'block-templates/navigation-site-vertical', $post_type ); ?>
 	<!-- SITE WRAPPER:START -->
 	<div class="wsu-wrapper-site">
 		<!-- SITE CONTAINER:START -->
-		<?php Template::render( 'block-templates/header-site', get_post_type() ); ?>
-		<?php Template::render( 'block-templates/navigation-site-horizontal', get_post_type() ); ?>
-		<?php Template::render( 'template-parts/archive', get_post_type() ); ?>
-		<?php get_template_part( 'template-parts/footer-site', get_post_type() ); ?>
+		<?php Template::render( 'block-templates/header-site', $post_type ); ?>
+		<?php Template::render( 'block-templates/navigation-site-horizontal', $post_type ); ?>
+		<?php Template::render( 'template-parts/archive', $post_type ); ?>
+		<?php get_template_part( 'template-parts/footer-site', $post_type ); ?>
 		<!-- SITE CONTAINER:END -->
 	</div>
 	<!-- SITE WRAPPER:END -->
-    <?php Template::render( 'block-templates/footer-global', get_post_type() ); ?>
+    <?php Template::render( 'block-templates/footer-global', $post_type ); ?>
 </div>
 <!-- GLOBAL CONTAINER:END -->
 <?php get_footer(); ?>
--- includes/theme.php
+++ includes/theme.php
@@ -4,7 +4,7 @@
 class Theme {
 
 
-	protected static $version = '1.0.4.1';
+	protected static $version = '1.0.5';
 
 
 	public static function get( $property ) {
